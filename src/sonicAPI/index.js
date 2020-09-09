a2bd04ec1726af70221b195578ffa08a3302e1a5
Nguyễn Vĩnh Thanh Chương <nvtc.98@gmail.com>
2020-09-09 11:31:03 +0700

    deploy

--- src/sonicAPI/index.js
+++ src/sonicAPI/index.js
@@ -7,8 +7,8 @@ exports.sendSonicRequest=async(cb) => {
 	var parameters = { blocking: false, format: 'json', access_id: sonicAccessId };
 
 	// the values for these parameters were taken from the corresponding controls in the demo form
-	// parameters['input_file'] = 'http://www.sonicAPI.com/music/brown_eyes_by_ueberschall.mp3';
-	parameters['input_file'] = heroku.url+'test.mp3';
+	parameters['input_file'] = 'http://www.sonicAPI.com/music/brown_eyes_by_ueberschall.mp3';
+	// parameters['input_file'] = heroku.url+'test.mp3';
 
 	console.log('start connecting api', parameters);
 	const result = await axios.get('https://api.sonicAPI.com/' + taskUrl, {
